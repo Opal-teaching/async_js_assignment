8e864502a9e8af7bc464a0fb43728418c91d79f6
David Herrera <davidfherrerar@gmail.com>
2018-05-10 15:23:40 -0400

    Extra question

---
+++ src/extra_question.js
@@ -0,0 +1,11 @@
+var s = require("./dumb_async_api"); // Async API
+var q = require("q");
+
+
+/**
+ *  Exercise 7:
+ *  Implement a function that acts like q.all, do not bother to implement the general case,
+ *  only implement it for the specific case of these five promises.
+ *
+ *
+ **/
