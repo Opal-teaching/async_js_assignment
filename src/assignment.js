5893f7ced0c7899c2d48aee63c5289e69ba6f95d
Thomas <Thomas@Thomass-MacBook-Air.local>
2018-05-11 11:02:49 -0400

    Solution to assignment

--- src/assignment.js
+++ src/assignment.js
@@ -1,5 +1,8 @@
 var dumb_async_api = require("./dumb_async_api"); // Async API
 var q = require("q"); // Promises package Node.js
+// ex1();
+// ex2();
+ex3();
 
 /* Exercise: 1
 *    Execute all the functions offered by the 'dumb_async_api' module a
@@ -7,6 +10,13 @@ var q = require("q"); // Promises package Node.js
 *    control over them at the moment.
 **/
 
+function ex1(){
+    dumb_async_api.async1();
+    dumb_async_api.async2();
+    dumb_async_api.async3();
+    dumb_async_api.async4();
+    dumb_async_api.async5();
+}
 
 /**
 * Exercise 2:
@@ -16,6 +26,53 @@ var q = require("q"); // Promises package Node.js
  *   deferred.resolve('async1') for instance.
 * */
 
+function ex2(){
+    async1promisified();
+    async2promisified();
+    async3promisified();
+    async4promisified();
+    async5promisified();
+}
+
+function async1promisified(){
+	var deferred1 = q.defer();
+	setTimeout(function(){
+		deferred1.resolve(console.log('async1'));
+	}, 5*Math.random());
+	return deferred1.promise;
+}
+
+function async2promisified(){
+    var deferred2 = q.defer();
+    setTimeout(function(){
+        deferred2.resolve(console.log('async2'));
+    }, 5*Math.random());
+    return deferred2.promise;
+}
+
+function async3promisified(){
+    var deferred3 = q.defer();
+    setTimeout(function(){
+        deferred3.resolve(console.log('async3'));
+    }, 5*Math.random());
+    return deferred3.promise;
+}
+
+function async4promisified(){
+    var deferred4 = q.defer();
+    setTimeout(function(){
+        deferred4.resolve(console.log('async4'));
+    }, 5*Math.random());
+    return deferred4.promise;
+}
+
+function async5promisified(){
+    var deferred5 = q.defer();
+    setTimeout(function(){
+        deferred5.resolve(console.log('async5'));
+    }, 5*Math.random());
+    return deferred5.promise;
+}
 
 /**
  * Exercise 3:
@@ -27,7 +84,38 @@ var q = require("q"); // Promises package Node.js
  * Note: If you want to catch the error with only one catch clause,
  * return the next promise as you progress through the promises.
  **/
-var result_str = "";
+
+
+function ex3(){
+
+    async1promisified().then(function(result_str){
+        result_str = 'async1, ';
+        return result_str
+    }).then(function(result_str){
+        async2promisified();
+        result_str += 'async2, ';
+        console.log(result_str);
+        return result_str
+    }).then(function(result_str){
+        async3promisified();
+        result_str += 'async3, ';
+        console.log(result_str);
+        return result_str
+    }).then(function(result_str){
+        async4promisified();
+        result_str += 'async4, ';
+        console.log(result_str);
+        return result_str
+    }).then(function(result_str){
+        async5promisified();
+        result_str += 'async5';
+        console.log(result_str);
+        return result_str
+    }).catch(function(){
+        alert(error);
+    })
+}
+
 /**
  * Exercise 4:
  * Use your current promisify API functions,
@@ -36,6 +124,10 @@ var result_str = "";
  * print 'done', at the end.
  * Expected Output: "async1, async2, async3, async4, async5"
  **/
+
+function ex4(){
+
+}
 
 /**
  * Exercise 5:
